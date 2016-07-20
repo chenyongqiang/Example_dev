b05d7c80f215791db4f11398350bac902b4dd6e4
qhurrican <qhurrican@163.com>
2016-07-20 13:57:50 +0800

    Initial commit

---
+++ app/src/main/java/getapkinfo/example/com/getapkinfo/GetApkInfoActivity.java
@@ -0,0 +1,13 @@
+package getapkinfo.example.com.getapkinfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GetApkInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_apk_info);
+    }
+}
