f6c822451d9219fbbbfef5beea3714483c1f53b8
qhurrican <qhurrican@163.com>
2016-08-04 17:21:00 +0800

    example_dev:ndk编译不过

--- app/src/main/jni/getapkinfo_example_com_getapkinfo_JNIUtils.cpp
+++ app/src/main/jni/getapkinfo_example_com_getapkinfo_JNIUtils.cpp
@@ -8,7 +8,7 @@
 #include "getapkinfo_example_com_getapkinfo_JNIUtils.h"
 /* Header for class getapkinfo_example_com_getapkinfo_JNIUtils */
 
-JNIEXPORT jstring JNICALL Java_getapkinfo_example_com_getapkinfo_JNIUtils_getPackname(JNIEnv *env, jobject clazz, jobject obj)
+JNIEXPORT jstring JNICALL Java_getapkinfo_example_com_getapkinfo_JNIUtils_getPackname(JNIEnv *env, jclass clazz, jobject obj)
 {
     jclass native_class = env->GetObjectClass(obj);
     jmethodID mId = env->GetMethodID(native_class, "getPackageName", "()Ljava/lang/String;");
@@ -16,20 +16,20 @@ JNIEXPORT jstring JNICALL Java_getapkinfo_example_com_getapkinfo_JNIUtils_getPac
     return packName;
 }
 
-JNIEXPORT jstring JNICALL Java_getapkinfo_example_com_getapkinfo_JNIUtils_getSignature(JNIEnv *env, jobject clazz, jobject obj)
+JNIEXPORT jstring JNICALL Java_getapkinfo_example_com_getapkinfo_JNIUtils_getSignature(JNIEnv *env, jclass clazz, jobject obj)
 {
     jclass native_class = env->GetObjectClass(obj);
     jmethodID pm_id = env->GetMethodID(native_class, "getPackageManager", "()Landroid/content/pm/PackageManager;");
     jobject pm_obj = env->CallObjectMethod(obj, pm_id);
     jclass pm_clazz = env->GetObjectClass(pm_obj);
-// 得到 getPackageInfo 方法的 ID
+//
     jmethodID package_info_id = env->GetMethodID(pm_clazz, "getPackageInfo","(Ljava/lang/String;I)Landroid/content/pm/PackageInfo;");
-    jstring pkg_str = Java_com_losileeya_getapkinfo_JNIUtils_getPackname(env, clazz, obj);
-// 获得应用包的信息
+    jstring pkg_str = Java_getapkinfo_example_com_getapkinfo_JNIUtils_getPackname(env, clazz, obj);
+//
     jobject pi_obj = env->CallObjectMethod(pm_obj, package_info_id, pkg_str, 64);
-// 获得 PackageInfo 类
+//
     jclass pi_clazz = env->GetObjectClass(pi_obj);
-// 获得签名数组属性的 ID
+//
     jfieldID signatures_fieldId = env->GetFieldID(pi_clazz, "signatures", "[Landroid/content/pm/Signature;");
     jobject signatures_obj = env->GetObjectField(pi_obj, signatures_fieldId);
     jobjectArray signaturesArray = (jobjectArray)signatures_obj;
